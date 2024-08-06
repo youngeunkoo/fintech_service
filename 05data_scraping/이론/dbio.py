743c55a1bc0252dce6cc6af593208089ac2efad8
youngeunkoo <kyy2206@naver.com>
2024-08-06 17:47:40 +0900

    0806

--- 05data_scraping/이론/dbio.py
+++ 05data_scraping/이론/dbio.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from exdbinfo import db, dbtype, id, pw, host, database   
 # MySQL 의 database="hanabank_info" 에 테이블 저장됨 
-from exdbinfo import db, dbtype, id, pw, host, news_database  
-from naver_api_info import news_database
+# from exdbinfo import db, dbtype, id, pw, host, news_database  
+# from naver_api_info import news_database
 import pymysql
 import time
 
@@ -27,17 +27,17 @@ def exi_to_db(table_name, date, df):
 
 
 
-# 네이버 뉴스용
-# sql 불러와서 아이디랑 비번 
-def news_db_connect():
-    engine = create_engine("%s+%s://%s:%s@%s/%s" % (db, dbtype, id, pw, host, news_database))
-    conn = engine.connect()
-    return conn
+# # 네이버 뉴스용
+# # sql 불러와서 아이디랑 비번 
+# def news_db_connect():
+#     engine = create_engine("%s+%s://%s:%s@%s/%s" % (db, dbtype, id, pw, host, news_database))
+#     conn = engine.connect()
+#     return conn
 
-# DB에 접속해서 저장하는 함수
-def news_to_db(table_name, date, df):
-    conn = news_db_connect()
-    df.to_sql(table_name, con=conn, if_exists='append', index=False) 
-    conn.close()
+# # DB에 접속해서 저장하는 함수
+# def news_to_db(table_name, date, df):
+#     conn = news_db_connect()
+#     df.to_sql(table_name, con=conn, if_exists='append', index=False) 
+#     conn.close()
     
-    return print(f"{table_name}_{date}, {'저장완료':<30s}", end="\r")
+#     return print(f"{table_name}_{date}, {'저장완료':<30s}", end="\r")
--- 05data_scraping/이론/exdbinfo.py
+++ 05data_scraping/이론/exdbinfo.py
@@ -4,3 +4,5 @@ id="root"
 pw="1234"
 host="127.0.0.1:3306"
 database="hanabank_info"
+
+news_database="fintech_news"
